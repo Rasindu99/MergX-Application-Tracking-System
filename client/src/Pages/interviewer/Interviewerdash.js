7e917d98b734e0549fef696a54324d2efdc05afc
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-03-26 23:46:59 +0530

    interviewer (error fix)
    error fix hiring manager -> interwierer

--- client/src/Pages/interviewer/Interviewerdash.js
+++ client/src/Pages/interviewer/Interviewerdash.js
@@ -4,7 +4,7 @@ import Logout from '../../Components/Logout'
 export default function Interviewerdash() {
   return (
     <div>
-      <h1 className='text-5xl'>Hiring Manager</h1>
+      <h1 className='text-5xl'>Interviewer</h1>
       <div className='pt-12'>
         <Logout/>
       </div>
