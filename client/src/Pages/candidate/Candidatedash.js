2c88f4def1fd5e79e0161db5db32eb097e39566c
Rasindu99 <rasindu0823@gmail.com>
2024-03-31 23:29:01 +0530

    Test1
    Temporary Update Candidate

--- client/src/Pages/candidate/Candidatedash.js
+++ client/src/Pages/candidate/Candidatedash.js
@@ -4,7 +4,7 @@ import Logout from '../../Components/Logout'
 export default function Candidatedash() {
   return (
     <div>
-      <h1 className='text-5xl'>candidate</h1>
+      <h1 className='text-5xl'>candidate 123</h1>
       <div className='pt-12'>
         <Logout/>
       </div>
