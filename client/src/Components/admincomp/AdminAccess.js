18e8a6848a2face8f6f4992b703b74ca29693572
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-04-30 16:12:55 +0530

    mofdify

--- client/src/Components/admincomp/AdminAccess.js
+++ client/src/Components/admincomp/AdminAccess.js
@@ -10,15 +10,65 @@ export default function AdminAccess() {
   return (
     <div className='flex justify-center'>
       <h1>Admin Access</h1>
-    <div className='flex border'>
+      <div>
+      <div className='flex border'>
         <label>
             Create User Account
         </label>
         <label>
             <input
             type="radio"
-            value="option1"
-            checked={selectedOption === "option1"}
+            value="createon"
+            checked={selectedOption === "createon"}
+            onChange={handleOptionChange}
+            />
+            Turn On
+        </label>
+        <label>
+            <input
+            type="radio"
+            value="createoff"
+            checked={selectedOption === "createoff"}
+            onChange={handleOptionChange}
+            />
+            Turn Off
+        </label>
+    </div>
+    
+    <div>
+      <label>
+        Modify  User Account
+      </label>
+      <label>
+            <input
+            type="radio"
+            value="modifyon"
+            checked={selectedOption === "modifyon"}
+            onChange={handleOptionChange}
+            />
+            Turn On
+        </label>
+        <label>
+            <input
+            type="radio"
+            value="modifyoff"
+            checked={selectedOption === "modifyoff"}
+            onChange={handleOptionChange}
+            />
+            Turn Off
+        </label>
+    </div>
+
+     
+    <div>
+      <label>
+        Role Assignment
+      </label>
+      <label>
+            <input
+            type="radio"
+            value="roleon"
+            checked={selectedOption === "roleon"}
             onChange={handleOptionChange}
             />
             Turn On
@@ -27,12 +77,40 @@ export default function AdminAccess() {
             <input
             type="radio"
             value="option2"
-            checked={selectedOption === "option2"}
+            checked={selectedOption === "roleoff"}
             onChange={handleOptionChange}
             />
             Turn Off
         </label>
     </div>
+
+     
+    <div>
+      <label>
+        Delete user account
+      </label>
+      <label>
+            <input
+            type="radio"
+            value="option1"
+            checked={selectedOption === "deleteon"}
+            onChange={handleOptionChange}
+            />
+            Turn On
+        </label>
+        <label>
+            <input
+            type="radio"
+            value="option2"
+            checked={selectedOption === "deleteoff"}
+            onChange={handleOptionChange}
+            />
+            Turn Off
+        </label>
+    </div>
+    
+      </div>
+    
       
 
     </div>
