a163e992dbc4caa9f86c3e8999ca0bb759af3ab5
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-06-12 19:50:12 +0530

    view job

--- client/src/Pages/interviewer/Scheduling.js
+++ client/src/Pages/interviewer/Scheduling.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import InterviewNav from '../../Components/interviewercomp/InterviewNav';
 import Description from '../../Components/interviewercomp/InterviewerDes';
+import ViewJobButton from '../../Components/interviewercomp/ViewJobButton';
 
 export default function Scheduling() {
   const name = 'Interview Scheduling'; 
 
   return (
-    <div className='flex'>
+   
+
+
+<div className='flex w-screen '>
+      
       <div>
-        <InterviewNav />
+      <InterviewNav />
       </div>
-      <div>
-        <Description name={name} />
+      
+      <div className='w-screen '>
+        
+        
+        <div className='flex pt-8 pb-8 pl-5'>
+          <div className=''>
+          <Description name={name} />
+          </div>
+          
+          <div>
+            
+          </div>
+        </div>
+          
+        
+    <div className='mx-[70px] bg-gradient-to-b from-[#272727] to-[#17171A] rounded-[30px] mt-10 h-[800px]'>
+      <div className='pt-12'>
+        <div className='flex mx-14'>
+          <ViewJobButton/>
+        </div>
       </div>
     </div>
+       
+        
+                            
+       
+                
+              </div>
+    
+  </div>
   );
 }
