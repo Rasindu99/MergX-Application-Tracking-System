07d1351ca80a26b438b87aa566e6fe9576a047dd
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-06-14 01:27:29 +0530

    interviewjob invitation

---
+++ client/src/Components/recruitercomp/SendJobInvitation.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function SendJobInvitation() {
+  return (
+    <div>
+        <div className='flex py-8'>
+            <div className='w-1/2  h-[680px]'>
+                <div className=''>
+                    <h1 className='text-2xl opacity-40'>new job interview invitation</h1>  
+                </div>
+                
+            </div>
+            <div  className='w-1/2 border-l border-orange-500 h-[680px]'>
+                <div>
+                    <h1  className='text-2xl opacity-40'>sent job interview invitation</h1>  
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
