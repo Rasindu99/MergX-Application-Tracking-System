3190daadf93468f589e0eba0d880790a77b15cd2
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-06-27 00:28:11 +0530

    Interview join

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const { uploadApplication, getApplicationsGroupedByJobId, approveApplication, rejectApplication,getapprovedtruedata} = require('../controllers/applicationController');
+const { uploadApplication, getApplicationsGroupedByJobId, approveApplication, rejectApplication, getapprovedtruedata, getisjoinedtrue, updateisjoinedtrue} = require('../controllers/applicationController');
 const upload = require('../helpers/upload');
 
 router.use(
@@ -16,7 +16,11 @@ router.post('/uploadapplication', upload.single('cv'), uploadApplication);
 router.get('/getjobgroup',getApplicationsGroupedByJobId );
 router.put('/approveapplication/:id', approveApplication);
 router.put('/rejectapplication/:id', rejectApplication );
-router.get('/getapprovedapplication',getapprovedtruedata)
+router.get('/getapprovedapplication',getapprovedtruedata);
+
+router.get('/getapprovedisjoinedtrue', getisjoinedtrue);
+router.put('/updateisjoinedtrue/:id', updateisjoinedtrue );
+
 
 
 
