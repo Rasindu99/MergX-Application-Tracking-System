8637bf3e74488c0f8c6dac4ffe9cf4c722b6a944
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-06-28 19:50:05 +0530

    backend interviewer access

--- server/models/interviewerAccess.js
+++ server/models/interviewerAccess.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 const intervieweraccessSchema = new mongoose.Schema({
     interview_scheduling : {
-
+        type:Boolean,
+        default:true
     },
     join_interview : {
-
+        type:Boolean,
+        default:true
     },
-    Submit_evaluation : {
-
+    submit_evaluation : {
+        type:Boolean,
+        default:true
     },
     feedback_submission : {
-
+        type:Boolean,
+        default:true
     }
 });
 const Intervieweraccessmodel = mongoose.model('Intervieweraccess', intervieweraccessSchema);
