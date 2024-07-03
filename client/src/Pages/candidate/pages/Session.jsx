5fed6016e511f5b846102c0db3fcc26482ccbb9a
Rasindu Sandeep <rasindu0823@gmail.com>
2024-07-03 08:39:02 +0530

    Application_Submission

--- client/src/Pages/candidate/pages/Session.jsx
+++ client/src/Pages/candidate/pages/Session.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import InterviewLinkCard from '../../../Components/candidateComp/InterviewLinkCrad/InterviewLinkCard'
-import Home from '../../../Components/InternalChatComp/home/Home';
 
 const Session = () => {
   return (
     <div className='flex justify-center items-center p-12 h-full'>
-      {/* <Home /> */}
+     
       <InterviewLinkCard/>
     </div>
   )
