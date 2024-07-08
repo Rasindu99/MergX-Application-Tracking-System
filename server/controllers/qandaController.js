68804cd154a89863f0e4b20528f4d8d99eaf0606
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-08 20:31:16 +0530

    Update qandaController.js
    send email

--- server/controllers/qandaController.js
+++ server/controllers/qandaController.js
@@ -1,4 +1,5 @@
 const QandAmodel = require('../models/qanda');
+const {sendMail} = require('../helpers/sendMail')
 
 //post end point Q and A
 const postqanda =  async(req, res) => {
@@ -12,6 +13,21 @@ const postqanda =  async(req, res) => {
             read,
             sent
         });
+
+        sendMail(useremail, "Thank you for your message!", 
+            `Dear ${username},
+    
+    Thank you for reaching out to us. We have received your message and appreciate your interest.
+    
+    We will review your inquiry and get back to you as soon as possible. Our team is committed to providing timely responses, typically within 1-2 business days.
+    
+    If you have any urgent matters, please don't hesitate to contact us directly at 0710821694.
+    
+    Thank you for your patience and understanding.
+    
+    Best regards,
+    MergeX Team
+    (Application Tracking System)`)
 
         return res.status(201).json({ message: 'Updated successfully', qanda });
     } catch (error) {
