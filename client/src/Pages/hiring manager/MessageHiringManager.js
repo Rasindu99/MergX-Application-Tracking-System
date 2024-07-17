ed6974bb7e4478c2c3c4fea1ddc6c4d36f752c76
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-18 01:54:13 +0530

    Hiring Manager Chatbot

--- client/src/Pages/hiring manager/MessageHiringManager.js
+++ client/src/Pages/hiring manager/MessageHiringManager.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HiringmanagerNav from '../../Components/hiringManagerCompo/HiringManagerNav'
 import Topbar from '../../Components/hiringManagerCompo/Topbar'
+import AdminChatBotBottom from '../../Components/admincomp/AdminChatBotBottom'
 
 export default function MessageHiringManager() {
   return (
@@ -11,7 +12,10 @@ export default function MessageHiringManager() {
       <div className='w-screen lg:ml-[320px] md:ml-72 ml-[260px] '>
       <Topbar title='Message' ></Topbar>
       </div>
-      
+       {/* Move AdminChatBotBottom here and wrap it in a positioned div */}
+       <div className="absolute bottom-0 right-0 z-50">
+        <AdminChatBotBottom/>
+      </div>
     </div>
   )
 }
