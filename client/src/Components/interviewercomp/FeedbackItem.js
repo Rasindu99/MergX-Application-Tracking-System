94fe4e82498c8535649726a3162cd20615fa6c7f
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-07-18 23:33:46 +0530

    Update FeedbackItem.js

--- client/src/Components/interviewercomp/FeedbackItem.js
+++ client/src/Components/interviewercomp/FeedbackItem.js
@@ -4,6 +4,7 @@ import '../../Pages/interviewer/custom.css'
 import PopUp from './Popup';
 import StarRating from './StarRating';
 import axios from 'axios';
+import {toast} from 'react-hot-toast';
 
 const FeedbackItem = ({ profile, name, date, position, userID }) => {
   const [showPopup, setShowPopup] = useState(false);
@@ -27,6 +28,10 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
   }, [userID]);
 
   const togglePopup = () => {
+    if (!feedback) {
+      toast.error('Admin blocked temporarily');
+      return;
+    }
     setShowPopup(!showPopup);
   };
 
@@ -39,6 +44,7 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
   };
 
   const handleSubmit = async () => {
+    
     try {
       // Send feedback data to backend
       const response = await axios.post('http://localhost:8000/feedback', {
@@ -67,21 +73,38 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
     setComment('');
   };
 
+  //access
+  const [feedback, setfeedback] = useState(false);
+
+  useEffect(() => {
+    // Fetch the current state of join_interview from the backend
+    const fetchJoinInterviewState = async () => {
+      try {
+        const response = await axios.get('/access/feedbacksubmissionget');
+        setfeedback(response.data.feedback_submission);
+      } catch (error) {
+        console.error('Error fetching join interview state:', error);
+      }
+    };
+
+    fetchJoinInterviewState();
+  }, []);
+
   return (
     <div>
       <div id='interview-item' className='flex items-center justify-between px-8 py-4'>
         <div>
-          <img src={profile} className='w-14 h-14 rounded-full border-2 border-white' alt='Profile'></img>
+          <img src={profile} className='border-2 border-white rounded-full w-14 h-14' alt='Profile'></img>
         </div>
         <div className='text-left -translate-x-24 max-w-44 min-w-44'> 
           <h1>{name}</h1>
-          <p className='opacity-50 text-sm'>CANDIDATE</p>
+          <p className='text-sm opacity-50'>CANDIDATE</p>
         </div>
         <div className='-translate-x-24 max-w-36 min-w-36'>
-          <p className='opacity-50 text-base'>Date - {date}</p>
+          <p className='text-base opacity-50'>Date - {date}</p>
         </div>
         <div className='-translate-x-20 max-w-32 min-w-32'>
-          <p className='opacity-50 text-base'>{position}</p>
+          <p className='text-base opacity-50'>{position}</p>
         </div>
         <div>
           {existingFeedbackList.some(feedback => feedback.userId === userID) ? (
@@ -103,9 +126,9 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
       </div>
 
       {showPopup && (
-        <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-white bg-opacity-5 backdrop-blur z-50'>
+        <div className='fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-white bg-opacity-5 backdrop-blur'>
           <div id='pop-bg' className='bg-[#2B2B2BE5] opacity-90 relative'>
-            <MdOutlineClose size={25} className='absolute top-5 right-5 cursor-pointer' onClick={togglePopup} />
+            <MdOutlineClose size={25} className='absolute cursor-pointer top-5 right-5' onClick={togglePopup} />
             <div className='flex items-center justify-around'>
               <p className='text-2xl bold'>Submit Your Feedback</p>
               <PopUp
@@ -114,15 +137,15 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
                role = 'CANDIDATE'
               />
             </div>
-            <p className='text-xl semi-bold mt-5'>Rate Your Expeirience</p>
-            <p className='text-center text-lg mt-5 w-3/4 mx-auto'>We request you to share your feedback. This helps us to get the valuable point to the candidate</p>
+            <p className='mt-5 text-xl semi-bold'>Rate Your Expeirience</p>
+            <p className='w-3/4 mx-auto mt-5 text-lg text-center'>We request you to share your feedback. This helps us to get the valuable point to the candidate</p>
             <div className='mx-auto mt-5'>
               <StarRating onChange={handleRatingChange} />
             </div>
             <div className='flex flex-col items-center justify-start mt-8'>
               <p className='text-left'>Add a Comment</p>
               <textarea
-                className="block bg-white bg-opacity-5 border border-white border-opacity-25 resize-none px-4 py-2 rounded mt-5"
+                className="block px-4 py-2 mt-5 bg-white border border-white border-opacity-25 rounded resize-none bg-opacity-5"
                 placeholder="Enter your text here..."
                 style={{ width: '45rem', height: '10rem'}}
                 value={comment}
@@ -130,7 +153,7 @@ const FeedbackItem = ({ profile, name, date, position, userID }) => {
                 rows="4"
               ></textarea>
             </div>
-            <div className='flex items-center justify-center gap-3 mt-6 mx-auto'>
+            <div className='flex items-center justify-center gap-3 mx-auto mt-6'>
               <button className='w-40 h-12 bg-[#EA7122] text-lg rounded-xl' onClick={handleSubmit}>Submit</button>
               <button className='w-40 h-12 bg-[#EA7122] text-lg rounded-xl bg-opacity-20' onClick={handleClear}>Clear</button>
             </div>
